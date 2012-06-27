b2b110f9a46f820b2a279c3cda518d0c51c4109d
Niklas Fischer <niklasfi@gmail.com>
2012-06-28 01:08:43 +0200

    basically cloned this from wiki/Plugin-API-Hooks

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ ep_base
 =======
 
 A sample etherpad-lite plugin
+
+# notes
+    replace all CAPITALIZED names with whatever you want
+
+# installing
+
+    npm install PATH/TO/THIS/FOLDER (from your etherpad-lite folder)
---
+++ YOURFILE.js
@@ -0,0 +1,11 @@
+exports.FUNCTIONNAME1 = function(req, res, next, ressource){
+    …
+}
+
+exports.FUNCTIONNAME2 = function(req, res, next, username, password){
+    …
+}
+
+exports.FUNCTIONNAME3 = function(hook_name, args,cb){
+    …
+}
