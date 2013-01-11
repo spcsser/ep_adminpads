9dbfd2cbefaa6867891607e69da85f44a3e0b409
spcsser <swen.jung@psx-technology.com>
2013-01-11 16:14:00 +0100

    First list and search functionality

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var eejs = require('ep_etherpad-lite/node/eejs')
   , padManager = require('ep_etherpad-lite/node/db/PadManager')
 ;
+RegExp.quote = require('regexp-quote')
+var isNumeric=function(arg){
+  return typeof(arg)=="number";
+};
 
 var pads={
   pads:[] ,
   search: function(query, callback){
+    
     var pads=padManager.getPads()
       , data={
         progress : 1
@@ -12,9 +17,35 @@ var pads={
         , query: query
         , total: Math.ceil(pads.length/12)
       }
+      , maxResult=0
+      , result=[]
     ;
-    data.results=pads.slice(query['offset'],query['limit']);
+    if(query["pattern"] != null && query["pattern"] != ''){
+      var pattern=query.pattern+"*";
+      pattern=RegExp.quote(pattern);
+      pattern=pattern.replace(/(\\\*)+/g,'.*');
+      pattern="^"+pattern+"$";
+      var regex=new RegExp(pattern,"i");
+      pads.forEach(function(padID){
+        if(regex.test(padID)){
+          result.push(padID);
+        }
+      });
+    }else{
+      result=pads;
+    }
+    
+    maxResult=result.length-1;
+    if(maxResult<0)maxResult=0;
+    
+    if(!isNumeric(query.offset) || query.offset<0) query.offset=0;
+    else if(query.offset>maxResult) query.offset=maxResult;
+    
+    if(!isNumeric(query.limit) || query.limit<0) query.limit=12;
+    
+    data.results=result.slice(query.offset, query.limit);
     pads.pads=data.results;
+    
     callback(data);
   }
 };
@@ -31,11 +62,12 @@ exports.registerRoute = function (hook_name, args, cb) {
 };
 
 exports.socketio = function (hook_name, args, cb) {
-  var io = args.io.of("/pluginfw/pads");
+  var io = args.io.of("/pluginfw/admin/pads");
   io.on('connection', function (socket) {
     if (!socket.handshake.session.user || !socket.handshake.session.user.is_admin) return;
 
     socket.on("load", function (query) {
+      console.log("load, query: %s",query);
       pads.search({parttern:'', offset:0, limit:12}, function (progress) {
         socket.emit("search-result", progress);
       });
@@ -50,5 +82,6 @@ exports.socketio = function (hook_name, args, cb) {
     socket.on("delete", function (plugin_name) {
       
     });
+    socket.on
   });
 };
--- static/js/admin/pads.js
+++ static/js/admin/pads.js
@@ -1,15 +1,17 @@
-$(document).ready(function(hooks, context){
-    var socket,
-    loc = document.location,
-    port = loc.port == "" ? (loc.protocol == "https:" ? 443 : 80) : loc.port,
-    url = loc.protocol + "//" + loc.hostname + ":" + port + "/",
-    pathComponents = location.pathname.split('/'),
-    // Strip admin/plugins
-    baseURL = pathComponents.slice(0,pathComponents.length-2).join('/') + '/',
-    resource = baseURL.substring(1) + "socket.io";
+exports.documentReady=function(hooks, context, cb){
+  if(context != 'admin/pads') return;
+  
+  var socket,
+  loc = document.location,
+  port = loc.port == "" ? (loc.protocol == "https:" ? 443 : 80) : loc.port,
+  url = loc.protocol + "//" + loc.hostname + ":" + port + "/",
+  pathComponents = location.pathname.split('/'),
+  // Strip admin/plugins
+  baseURL = pathComponents.slice(0,pathComponents.length-2).join('/') + '/',
+  resource = baseURL.substring(1) + "socket.io";
 
   //connect
-  socket = io.connect(url, {resource : resource}).of("/pluginfw/pads");
+  socket = io.connect(url, {resource : resource}).of("/pluginfw/admin/pads");
 
   $('.search-results').data('query', {
     pattern: '',
@@ -117,4 +119,5 @@ $(document).ready(function(hooks, context){
 
   socket.emit("load");
   search();
-});
+  return cb;
+};
